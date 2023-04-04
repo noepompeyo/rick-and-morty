47c44b7b1aef8c351df19cb00ea48857b5983e95
noepm <pompeyonoe@gmail.com>
2023-04-04 13:53:56 -0600

    Se arreglo un a palabra

--- src/js/Pages/Error404.js
+++ src/js/Pages/Error404.js
@@ -2,7 +2,7 @@ const Error404 = () => {
     const view = `
     <div class="Error404">
      <h2>Error 404</h2>
-     <h3>Esta paguina aun esta en proceso de creacion, da click en el logo de la pauina para volver al menu  </h3>
+     <h3>Esta paguina aun esta en proceso de creacion, da click en el logo de la paguina para volver al menu  </h3>
     </div>
     
     `;
