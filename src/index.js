191211b0e19b3b7ade9583f88fe8068b5df75605
noepm <pompeyonoe@gmail.com>
2023-04-24 00:05:26 -0600

    [ADD] scrollTop SVG

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ import router  from "./routes";
 
 window.addEventListener('load', router);
 window.addEventListener('hashchange',router)
+
+window.onscroll = function(){scrollFunction()};
+
+function scrollFunction() {
+    if ( document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById('arrow-scroll').style.display = 'block';
+        } else {
+            document.getElementById('arrow-scroll').style.display = 'none';
+        }
+
+        document.getElementById('arrow-scroll').onclick = function () {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+}
